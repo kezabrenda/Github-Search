22323a87e892623b2f04e4eadf9a8e3e510cac3b
keza1brenda <keza1brenda@gmail.com>
2020-11-09 00:31:14 +0200

    add gitSearch component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+//import { GitSearchComponent } from './git-search/git-search.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  //{ path: 'gitSearch', component: GitSearchComponent},
+  { path: 'about', component: AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
