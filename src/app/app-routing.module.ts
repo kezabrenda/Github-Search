82a120d10dfef6006382087ef682960200e471f7
keza1brenda <keza1brenda@gmail.com>
2020-11-09 00:48:22 +0200

    add routing component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-//import { GitSearchComponent } from './git-search/git-search.component';
+import { GitSearchComponent } from './git-search/git-search.component';
+import { RoutingComponent } from './routing/routing.component';
 
 const routes: Routes = [
-  //{ path: 'gitSearch', component: GitSearchComponent},
+  { path: 'gitSearch', component: GitSearchComponent},
+  { path: 'routing', component: RoutingComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GitSearchComponent } from './git-search/git-search.component';
+import { RoutingComponent } from './routing/routing.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    GitSearchComponent,
+    RoutingComponent,
   ],
   imports: [
     BrowserModule,
