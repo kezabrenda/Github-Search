dbf1bc560e47901767874e699aa3efc73120adac
keza1brenda <keza1brenda@gmail.com>
2020-11-09 00:35:30 +0200

    add gitSearch component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 //import { GitSearchComponent } from './git-search/git-search.component';
-import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   //{ path: 'gitSearch', component: GitSearchComponent},
-  { path: 'about', component: AboutComponent},
 ];
 
 @NgModule({
--- src/app/git-search/git-search.component.spec.ts
+++ src/app/git-search/git-search.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AboutComponent } from './about.component';
+import { GitSearchComponent } from './git-search.component';
 
-describe('AboutComponent', () => {
-  let component: AboutComponent;
-  let fixture: ComponentFixture<AboutComponent>;
+describe('GitSearchComponent', () => {
+  let component: GitSearchComponent;
+  let fixture: ComponentFixture<GitSearchComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AboutComponent ]
+      declarations: [ GitSearchComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AboutComponent);
+    fixture = TestBed.createComponent(GitSearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
